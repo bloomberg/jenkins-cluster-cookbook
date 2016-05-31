c455ae34a83d48454ae32b2487bfb78bb8a5e237
John Bellone <jbellone@bellone.us>
2016-05-30 21:01:11 -0400

    Update kitchen.yml and fixtures for policyfiles.

--- test/fixtures/policies/master.rb
+++ test/fixtures/policies/master.rb
@@ -1,4 +1,8 @@
-name 'master'
+name 'default'
 default_source :community
 run_list 'jenkins-cluster::master'
 cookbook 'jenkins-cluster', path: '../../../'
+cookbook 'ubuntu'
+cookbook 'redhat'
+named_run_list :redhat, 'redhat::default', 'jenkins-cluster::master'
+named_run_list :ubuntu, 'ubuntu::default', 'jenkins-cluster::master'
