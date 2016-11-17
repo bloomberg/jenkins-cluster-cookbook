6fef68d8d5c70f9d8c25534a0e41fad476f742a8
John Bellone <jbellone@bloomberg.net>
2016-11-17 09:07:13 -0500

    Update java options.

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,7 +11,4 @@ default['jenkins']['service_name'] = 'jenkins'
 default['jenkins']['service_user'] = 'jenkins'
 default['jenkins']['service_group'] = 'jenkins'
 default['jenkins']['service_home'] = '/var/lib/jenkins'
-default['jenkins']['java_options'] = '-Djava.awt.headless=true -Xmx2048m'
-
-default['jenkins']['docker']['group'] = 'docker'
-default['jenkins']['docker']['bip'] = '192.18.0.1/15'
+default['jenkins']['java_options'] = '-Xmx2048m -XX:MaxPermSize=512m -Djava.awt.headless=true'
